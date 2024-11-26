be903e44644225bdd11471841df476e1dd0e88e1
pudge2002 <silverhof634@gmail.com>
2024-11-26 21:45:49 +0300

    1

---
+++ gameHub/src/test/java/gameHub/GameHubApplicationTests.java
@@ -0,0 +1,13 @@
+package gameHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
