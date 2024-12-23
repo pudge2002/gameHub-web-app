f81fb50eb421d972a949b19eb453c2ac074c8815
pudge2002 <silverhof634@gmail.com>
2024-12-23 08:36:04 +0300

    поправил чарты

--- GameHub/src/main/java/GameHub/views/pages/DashboardView.java
+++ GameHub/src/main/java/GameHub/views/pages/DashboardView.java
@@ -1,9 +1,7 @@
 package GameHub.views.pages;
 
 import com.vaadin.flow.component.charts.Chart;
-import com.vaadin.flow.component.charts.model.ChartType;
-import com.vaadin.flow.component.charts.model.DataSeries;
-import com.vaadin.flow.component.charts.model.DataSeriesItem;
+import com.vaadin.flow.component.charts.model.*;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -95,27 +93,44 @@ public class DashboardView extends VerticalLayout {
     }
 
     private Chart createGenreChart() {
-        Chart chart = new Chart(ChartType.BAR);
+        Chart chart = new Chart(ChartType.COLUMN);
+
+        Configuration configuration = chart.getConfiguration();
+        configuration.setTitle("Игры по жанрам");
+
+        XAxis xAxis = new XAxis();
+        xAxis.setType(AxisType.CATEGORY); // Устанавливаем тип оси X как категориальный
+        configuration.addxAxis(xAxis);
+
+        YAxis yAxis = new YAxis();
+        yAxis.setTitle("Количество игр");
+        configuration.addyAxis(yAxis);
 
         DataSeries dataSeries = new DataSeries();
         List<Game> games = gameRepository.findAll();
 
         // Вывод информации о играх в консоль
+        System.out.println("Games:");
         games.forEach(game -> System.out.println(game.getName() + " - " + game.getGenre()));
 
         Map<String, Long> genreGameCount = games.stream()
                 .collect(Collectors.groupingBy(Game::getGenre, Collectors.counting()));
 
         // Вывод информации о количестве игр по жанрам в консоль
+        System.out.println("Genre Game Counts:");
         genreGameCount.forEach((genre, count) -> System.out.println(genre + ": " + count));
+
+        // Установка категорий оси X
+        xAxis.setCategories(genreGameCount.keySet().toArray(new String[0]));
 
         for (Map.Entry<String, Long> entry : genreGameCount.entrySet()) {
             dataSeries.add(new DataSeriesItem(entry.getKey(), entry.getValue().doubleValue()));
+            System.out.println("Adding to chart: " + entry.getKey() + " with game count: " + entry.getValue());
         }
 
-        chart.getConfiguration().setSeries(dataSeries);
-        chart.getConfiguration().setTitle("Игры по жанрам");
+        configuration.setSeries(dataSeries);
 
         return chart;
     }
+
 }
